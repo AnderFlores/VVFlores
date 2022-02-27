98b00577e103e905ec3725023fd3034fe80c5b36
Ander Flores <flores.ander29@gmail.com>
2022-02-27 14:13:47 +0100

    Añadir test when_negative_and_non_negative_numbers_given_returns_exception

--- stringcalculatorkata/tests/StringCalculatorKataTest.php
+++ stringcalculatorkata/tests/StringCalculatorKataTest.php
@@ -100,4 +100,17 @@ class StringCalculatorKataTest extends TestCase
             $this->assertEquals("Negative not allowed : -1, -2", $ex->getMessage());
         }
     }
+
+    /**
+     * @test
+     */
+    public function when_negative_and_non_negative_numbers_given_returns_exception()
+    {
+        $numbers = "-1,-2,3";
+        try {
+            $result = $this->stringCalculatorKata->add($numbers);
+        } catch (Exception $ex) {
+            $this->assertEquals("Negative not allowed : -1, -2", $ex->getMessage());
+        }
+    }
 }
