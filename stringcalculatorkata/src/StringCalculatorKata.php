9a9b9c5faf6d47dae4f41e1436edbe24b83f3a3f
Ander Flores <flores.ander29@gmail.com>
2022-02-27 17:32:18 +0100

    Añadido funcionalidad para utilizar delimitadores especiales

--- stringcalculatorkata/src/StringCalculatorKata.php
+++ stringcalculatorkata/src/StringCalculatorKata.php
@@ -11,13 +11,38 @@ class StringCalculatorKata
         if (empty($numbers)) {
             return "0";
         }
-        $arrayNumbers = preg_split('/[,|\n]/', $numbers);
+        $delimiter = ',';
+        if ($this->checkCustomDelimiter($numbers)) {
+            $delimiter = $this->getCustomDelimiter($numbers);
+            $numbers = $this->getNumbers($numbers);
+        }
+        $arrayNumbers = $this->splitNumbersByDelimiters($numbers, $delimiter);
         $this->checkNegatives($arrayNumbers);
         $result = 0;
-        for ($i = 0; $i < sizeof($arrayNumbers); $i++) {
-            $result += $arrayNumbers[$i];
+        foreach ($arrayNumbers as $number) {
+            $result += $number;
         }
         return $result;
+    }
+
+    private function checkCustomDelimiter(String $numbers): bool
+    {
+        return str_starts_with($numbers, "//");
+    }
+
+    private function getCustomDelimiter(String $numbersWithCustomDelimiter): String
+    {
+        return substr($numbersWithCustomDelimiter, 2, strpos($numbersWithCustomDelimiter, "\n") - 1);
+    }
+
+    private function splitNumbersByDelimiters(String $numbers, String $delimiters): array
+    {
+        return preg_split('/[' . $delimiters . '\n]/', $numbers);;
+    }
+
+    private function getNumbers(String $numbersWithCustomDelimiter): String
+    {
+        return substr($numbersWithCustomDelimiter, strpos($numbersWithCustomDelimiter, "\n") + 1);
     }
 
     private function checkNegatives(array $arrayNumbers): void
